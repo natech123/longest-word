e9994aeb5100ae204d8ba85737d5afb866d18cf5
nathaniel casely hayford <chnate99@gmail.com>
2022-12-20 13:05:15 +0000

    Feature complete: Dictionary check of attempt

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,11 +1,24 @@
 import requests
+import string
+import random
+
 
 class Game:
-    # [...]
+    """ Class for game instances"""
+    def __init__(self) -> list:
+        """Attribute a random grid to size 9"""
+        self.grid = random.choices(string.ascii_uppercase, k = 9)
 
     def is_valid(self, word):
-        # [...]
-
+            # [...]
+        if not word:
+            return False
+        letters = self.grid.copy() # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
         return self.__check_dictionary(word) # instead of return True
 
 
@@ -15,30 +28,3 @@ class Game:
         json_response = response.json()
         return json_response['found']
 
-
-
-# import string
-# import random
-
-
-# class Game:
-#     """ Class for game instances"""
-#     def __init__(self) -> list:
-#         """Attribute a random grid to size 9"""
-#         self.grid = random.choices(string.ascii_uppercase, k = 9)
-
-
-#     def is_valid(self, word: str) -> bool:
-#         """Return True if and only if the word is valid, given the Game's grid"""
-#         # combine = list(word) + self.grid
-
-#         # return len(set(combine)) == 9
-#         if not word:
-#             return False
-#         letters = self.grid.copy() # Consume letters from the grid
-#         for letter in word:
-#             if letter in letters:
-#                 letters.remove(letter)
-#             else:
-#                 return False
-#         return True
