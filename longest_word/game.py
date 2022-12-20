4ca4488a4c25884335a14ecaf827d897e5847e10
nathaniel casely hayford <chnate99@gmail.com>
2022-12-20 12:03:12 +0000

    Feature complete: Dictionary check of attempt

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,25 +1,44 @@
-import string
-import random
-
+import requests
 
 class Game:
-    """ Class for game instances"""
-    def __init__(self) -> list:
-        """Attribute a random grid to size 9"""
-        self.grid = random.choices(string.ascii_uppercase, k = 9)
+    # [...]
+
+    def is_valid(self, word):
+        # [...]
+
+        return self.__check_dictionary(word) # instead of return True
 
 
-    def is_valid(self, word: str) -> bool:
-        """Return True if and only if the word is valid, given the Game's grid"""
-        # combine = list(word) + self.grid
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
 
-        # return len(set(combine)) == 9
-        if not word:
-            return False
-        letters = self.grid.copy() # Consume letters from the grid
-        for letter in word:
-            if letter in letters:
-                letters.remove(letter)
-            else:
-                return False
-        return True
+
+
+# import string
+# import random
+
+
+# class Game:
+#     """ Class for game instances"""
+#     def __init__(self) -> list:
+#         """Attribute a random grid to size 9"""
+#         self.grid = random.choices(string.ascii_uppercase, k = 9)
+
+
+#     def is_valid(self, word: str) -> bool:
+#         """Return True if and only if the word is valid, given the Game's grid"""
+#         # combine = list(word) + self.grid
+
+#         # return len(set(combine)) == 9
+#         if not word:
+#             return False
+#         letters = self.grid.copy() # Consume letters from the grid
+#         for letter in word:
+#             if letter in letters:
+#                 letters.remove(letter)
+#             else:
+#                 return False
+#         return True
